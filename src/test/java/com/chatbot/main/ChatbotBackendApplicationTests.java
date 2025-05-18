b73acab0c16ae4a33db14df01281dafa941e5b48
796036 <navnathtarte123@gmail.com>
2025-05-18 23:20:28 +0530

    Initial commit

---
+++ src/test/java/com/chatbot/main/ChatbotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbot.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
